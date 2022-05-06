d54575efbec2d9056950f3a92fd929ef4671bd57
manny-e1 <aewnetu6@gmail.com>
2022-05-06 16:16:36 +0300

    join-game event

--- backend/index.js
+++ backend/index.js
@@ -56,6 +56,39 @@ io.on('connection', (socket) => {
       console.log(err);
     }
   });
+
+  socket.on('join-game', async ({ nickname, name }) => {
+    try {
+      let room = await Room.findOne({ name });
+      if (!room) {
+        socket.emit('notCorrectGame', 'Please enter a valid room name');
+        return;
+      }
+
+      if (room.isJoin) {
+        let player = {
+          socketID: socket.id,
+          nickname,
+        };
+        room.players.push(player);
+        socket.join(name);
+
+        if (room.players.length === room.occupancy) {
+          room.isJoin = false;
+        }
+        room.turn = room.players[room.turnIndex];
+        room = await room.save();
+        io.to(name).emit('updateRoom', room);
+      } else {
+        socket.emit(
+          'notCorrectGame',
+          'The game is in progress, please try later!'
+        );
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  });
 });
 
 server.listen(port, '0.0.0.0', () => {
