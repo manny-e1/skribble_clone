a57d8fe133ae3a636e059d43b567a32be1b2815a
manny-e1 <aewnetu6@gmail.com>
2022-05-06 15:00:42 +0300

    create room screen

---
+++ lib/create_room_screen.dart
@@ -0,0 +1,133 @@
+import 'package:flutter/material.dart';
+import 'package:skribbl_clone/widgets/custom_text_field.dart';
+
+class CreateRoomScreen extends StatefulWidget {
+  const CreateRoomScreen({Key? key}) : super(key: key);
+
+  @override
+  _CreateRoomScreenState createState() => _CreateRoomScreenState();
+}
+
+class _CreateRoomScreenState extends State<CreateRoomScreen> {
+  final _nameController = TextEditingController();
+  final _roomNameController = TextEditingController();
+  String? _maxRoundValue;
+  String? _maxRoomSize;
+
+  @override
+  void dispose() {
+    _nameController.dispose();
+    _roomNameController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Text(
+            'Create Room',
+            style: TextStyle(
+              color: Colors.black,
+              fontSize: 30,
+            ),
+          ),
+          SizedBox(
+            height: size.height * 0.08,
+          ),
+          Container(
+            margin: const EdgeInsets.symmetric(horizontal: 20),
+            child: CustomTextField(
+              controller: _nameController,
+              hintText: 'Enter your name',
+            ),
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          Container(
+            margin: const EdgeInsets.symmetric(horizontal: 20),
+            child: CustomTextField(
+              controller: _roomNameController,
+              hintText: 'Enter room name',
+            ),
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          DropdownButton<String>(
+            focusColor: const Color(0xffF5F6FA),
+            hint: Text(
+              _maxRoundValue ?? 'Select Max Rounds',
+              style: const TextStyle(
+                  color: Colors.black,
+                  fontSize: 14,
+                  fontWeight: FontWeight.w500),
+            ),
+            items: <String>['2', '5', '10', '15']
+                .map<DropdownMenuItem<String>>(
+                  (String value) => DropdownMenuItem(
+                    value: value,
+                    child: Text(value),
+                  ),
+                )
+                .toList(),
+            onChanged: (value) {
+              setState(() {
+                _maxRoundValue = value;
+              });
+            },
+          ),
+          const SizedBox(height: 20,),
+          DropdownButton<String>(
+            focusColor: const Color(0xffF5F6FA),
+            hint:  Text(
+              _maxRoomSize ?? 'Select Room Size  ',
+              style: const TextStyle(
+                  color: Colors.black,
+                  fontSize: 14,
+                  fontWeight: FontWeight.w500),
+            ),
+            items: <String>['2', '3', '4', '5','6','7','8']
+                .map<DropdownMenuItem<String>>(
+                  (String value) => DropdownMenuItem(
+                    value: value,
+                    child: Text(value),
+                  ),
+                )
+                .toList(),
+            onChanged: (value) {
+              setState(() {
+                _maxRoomSize = value;
+              });
+            },
+          ),
+          const SizedBox(height: 40,),
+   ElevatedButton(
+                onPressed: () {},
+                child: const Text(
+                  'Create',
+                  style: TextStyle(fontSize: 16),
+                ),
+                style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all(Colors.blue),
+                  textStyle: MaterialStateProperty.all(
+                    const TextStyle(color: Colors.white),
+                  ),
+                  minimumSize: MaterialStateProperty.all(
+                    Size(
+                      size.width / 2.5,
+                      50,
+                    ),
+                  ),
+                ),
+              ),
+        ],
+      ),
+    );
+  }
+}
--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:skribbl_clone/create_room_screen.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -26,27 +27,44 @@ class _HomeScreenState extends State<HomeScreen> {
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
               ElevatedButton(
-                onPressed: () {},
+                onPressed: () {
+                  Navigator.of(context).push(MaterialPageRoute(
+                      builder: (context) => const CreateRoomScreen()));
+                },
                 child: const Text(
                   'Create',
-                  style: TextStyle(fontSize:16),
+                  style: TextStyle(fontSize: 16),
                 ),
                 style: ButtonStyle(
                   backgroundColor: MaterialStateProperty.all(Colors.blue),
-                  textStyle: MaterialStateProperty.all(const TextStyle(color:Colors.white),),
-                  minimumSize: MaterialStateProperty.all(Size(size.width/2.5,50,),),
+                  textStyle: MaterialStateProperty.all(
+                    const TextStyle(color: Colors.white),
+                  ),
+                  minimumSize: MaterialStateProperty.all(
+                    Size(
+                      size.width / 2.5,
+                      50,
+                    ),
+                  ),
                 ),
               ),
               ElevatedButton(
                 onPressed: () {},
                 child: const Text(
                   'Join',
-                  style: TextStyle(fontSize:16),
+                  style: TextStyle(fontSize: 16),
                 ),
-                style:ButtonStyle(
+                style: ButtonStyle(
                   backgroundColor: MaterialStateProperty.all(Colors.blue),
-                  textStyle: MaterialStateProperty.all(const TextStyle(color:Colors.white),),
-                  minimumSize: MaterialStateProperty.all(Size(size.width/2.5,50,),),
+                  textStyle: MaterialStateProperty.all(
+                    const TextStyle(color: Colors.white),
+                  ),
+                  minimumSize: MaterialStateProperty.all(
+                    Size(
+                      size.width / 2.5,
+                      50,
+                    ),
+                  ),
                 ),
               ),
             ],
